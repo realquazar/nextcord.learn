adf5019774dace57be7b8e08ec7c96e9859379e2
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-06 01:10:23 +0530

    Update basic.py

--- setup/basic.py
+++ setup/basic.py
@@ -1,3 +1,17 @@
+'''
+In this code we:
+1) Declare the intents variable and set them to either default or all. Intents can also be enabled individually as shown below with 
+intents.typing. For more information refer to the documentation: https://docs.nextcord.dev/en/stable/intents.html
+
+2) Initialize the commands.Bot() class to a variable with a command prefix (>) and intents
+
+3) Add an event (on_ready) to display a message when the bot is logged in
+
+4) Create a command that greets the user who uses the command
+
+5) Run the bot, replace TOKEN with your actual token set in the discord developer portal
+'''
+
 import nextcord
 from nextcord.ext import commands
 
