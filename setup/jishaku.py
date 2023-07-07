72f00b32c216e5cec341ae84ca29bf2cd0e197e6
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-07 09:22:10 +0530

    Update jishaku.py

--- setup/jishaku.py
+++ setup/jishaku.py
@@ -1,4 +1,14 @@
-#In this code, we add the extension jishaku to help for logging with nextcord
+'''
+In this code, we add the extension jishaku to help for logging with nextcord
+To install jishaku use: 
+  Mac/linux: pip3 install jishaku
+  Windows: pip install jishaku
+
+To run jishaku in your server use
+>jsk sync
+
+Note: replace ">" with your prefix command
+'''
 
 import nextcord
 from nextcord.ext import commands
