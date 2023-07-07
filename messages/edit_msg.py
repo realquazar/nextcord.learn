c1c9677b1a995de51e75b8c8ee09dbd9075152af
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-07 09:00:23 +0530

    Update edit_msg.py

--- messages/edit_msg.py
+++ messages/edit_msg.py
@@ -1,1 +1,28 @@
+import nextcord
+from nextcord.ext import commands
+import asyncio
 
+intents = nextcord.Intents.all()
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"bot is online: {bot.user}")
+
+@bot.command()
+async def edit1(interaction: nextcord.Interaction, *, message: str):
+    message = await interaction.send(message)
+    asyncio.sleep(10)
+    await message.edit(content="This message has been edited")
+
+@bot.command()
+async def edit_with_id(interaction: nextcord.Interaction, channelid: nextcord.TextChannel, msgid: int):
+    if not channelid:
+        interaction.send("please provide a channel id")
+    elif not msgid:
+        interaction.send("Please provide the message id")
+    else:
+        message_to_edit = await channel.fetch_message(msgid)
+        await message_to_edit.edit(content="This message has been edited")
+
+bot.run("TOKEN")
