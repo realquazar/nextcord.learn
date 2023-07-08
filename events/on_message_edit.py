036de532d0cbb4fbed0a5bd24a176ee16d2d5dc0
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-08 21:48:31 +0530

    Create on_message_edit.py

---
+++ events/on_message_edit.py
@@ -0,0 +1,11 @@
+import nextcord
+from nextcord.ext import commands
+
+intents=nextcord.Intents.default()
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.event
+async def on_message_edit(before, after):
+    await after.channel.send(f'A message was edited by {after.author}: {before.content} -> {after.content}')
+
+bot.run("TOKEN")
