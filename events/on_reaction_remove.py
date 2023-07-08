ef63e2bc554b2d9ab66193e204232f6c6e6cd762
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-08 22:11:29 +0530

    Update on_reaction_remove.py

--- events/on_reaction_remove.py
+++ events/on_reaction_remove.py
@@ -3,5 +3,8 @@ from nextcord.ext import commands
 
 bot = commands.Bot(command_prefix=">", intents=nextcord.Intents.default())
 
+@bot.event
+async def on_reaction_remove(reaction, user):
+    await reaction.message.channel.send(f'{user.name} removed their reaction of {reaction.emoji}!')
 
 bot.run("TOKEN")
