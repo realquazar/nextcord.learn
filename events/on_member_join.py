53ef15b6e2d4f7e9e871d8f3fcbae0f129869a6d
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-08 22:07:00 +0530

    Update on_member_join.py

--- events/on_member_join.py
+++ events/on_member_join.py
@@ -1,7 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix=">", intents=nextcord.Intents.default())
 
 
 bot.run("TOKEN")
