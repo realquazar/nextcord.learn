0f23b7cea0811594f7087ae8fdbbaba92b1bf174
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-08 22:09:25 +0530

    Update on_member_join.py

--- events/on_member_join.py
+++ events/on_member_join.py
@@ -3,5 +3,9 @@ from nextcord.ext import commands
 
 bot = commands.Bot(command_prefix=">", intents=nextcord.Intents.default())
 
+@bot.event
+async def on_member_join(member):
+    welcome_channel = bot.get_channel(welcome_channel_id)
+    await welcome_channel.send(f'Welcome, {member.name}!')
 
 bot.run("TOKEN")
