f49a63453c7fc54cd86433021c382560df22b8f7
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-08 22:09:50 +0530

    Update on_member_remove.py

--- events/on_member_remove.py
+++ events/on_member_remove.py
@@ -3,5 +3,9 @@ from nextcord.ext import commands
 
 bot = commands.Bot(command_prefix=">", intents=nextcord.Intents.default())
 
+@bot.event
+async def on_member_remove(member):
+    goodbye_channel = bot.get_channel(goodbye_channel_id)
+    await goodbye_channel.send(f'Goodbye, {member.name}!')
 
 bot.run("TOKEN")
