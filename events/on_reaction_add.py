5322e49ebd38e081e976b955f1a3ac1ac3df6597
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-08 22:11:05 +0530

    Update on_reaction_add.py

--- events/on_reaction_add.py
+++ events/on_reaction_add.py
@@ -3,5 +3,8 @@ from nextcord.ext import commands
 
 bot = commands.Bot(command_prefix=">", intents=nextcord.Intents.default())
 
+@bot.event
+async def on_reaction_add(reaction, user):
+    await reaction.message.channel.send(f'{user.name} reacted with {reaction.emoji}!')
 
 bot.run("TOKEN")
