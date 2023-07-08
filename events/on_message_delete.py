01f686fae4f33552d1821355893bc729c50e0fc7
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-08 22:10:30 +0530

    Update on_message_delete.py

--- events/on_message_delete.py
+++ events/on_message_delete.py
@@ -3,5 +3,8 @@ from nextcord.ext import commands
 
 bot = commands.Bot(command_prefix=">", intents=nextcord.Intents.default())
 
+@bot.event
+async def on_message_delete(message):
+    await message.channel.send(f'A message was deleted: {message.content}')
 
 bot.run("TOKEN")
