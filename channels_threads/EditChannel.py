3fee7fe1cf9e8c75b5bf430d1cfa5e6e3e0f4073
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-11 15:11:03 +0530

    Update EditChannel.py

--- channels_threads/EditChannel.py
+++ channels_threads/EditChannel.py
@@ -1,1 +1,23 @@
+'''
+In this code we edit a channel
+'''
 
+import nextcord
+from nextcord.ext import commands
+
+intents = nextcord.Intents.default()
+intents.channels = True
+
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot is ready: {bot.user}')
+
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def edit_channel(ctx, channel: nextcord.TextChannel, new_name: str):
+    await channel.edit(name=new_name)
+    await ctx.send(f"Edited channel: {channel.mention} to {new_name}")
+
+bot.run("TOKEN")
