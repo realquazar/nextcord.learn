6628b81ba3458f386b30c75956a128272b20321b
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-11 15:16:03 +0530

    Create CreateThread.py

---
+++ channels_threads/CreateThread.py
@@ -0,0 +1,23 @@
+'''
+In this code we add a role
+'''
+
+import nextcord
+from nextcord.ext import commands
+
+intents = nextcord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot is ready: {bot.user}')
+
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def create_thread(ctx, channel: nextcord.TextChannel, thread_name: str):
+    thread = await channel.create_thread(name=thread_name)
+    await ctx.send(f"Created thread: {thread.mention}")
+      
+bot.run("TOKEN")
