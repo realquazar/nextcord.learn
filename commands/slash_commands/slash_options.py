91449b13e91c13a899d220df336129801736f8d0
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-14 19:35:31 +0530

    Update slash_options.py

--- commands/slash_commands/slash_options.py
+++ commands/slash_commands/slash_options.py
@@ -1,1 +1,22 @@
+'''
+In this code we create options for out slash command "hi"
 
+Note:
+Slash Options can be made optional by setting "required=False" in the parameter of SlashOption()
+'''
+
+import nextcord
+from nextcord.ext import commands
+
+bot = commands.Bot(command_prefix=">",  intents=nextcord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f"Bot connected as {bot.user}")
+    
+@bot.slash_command(name="hi", description="Say hi", number: int = SlashOption(name="picker", choices={"one": 1, "two": 2, "three": 3}))
+async def say_hi(interaction: nextcord.Interaction):
+    await interaction.send("Hi")
+
+
+bot.run("TOKEN")
