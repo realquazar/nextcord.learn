767388b9f3326703d31192082ae475f68fbd21ff
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-20 20:18:47 +0530

    Update guild_id.py

--- properties/guild_id.py
+++ properties/guild_id.py
@@ -1,1 +1,14 @@
+import nextcord
+from nextcord.ext import commands
 
+intents = nextcord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.command()
+async def get_guild_id(ctx: nextcord.commands.Context):
+    guild_id = ctx.guild.id
+    await ctx.send(f"Guild ID: {guild_id}")
+
+bot.run("TOKEN")
