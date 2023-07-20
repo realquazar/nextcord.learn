ddfa5d841ba2a2cbb204944cef547ad4919c39df
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-20 20:37:25 +0530

    Update update_bot_pfp.py

--- properties/update_bot_pfp.py
+++ properties/update_bot_pfp.py
@@ -1,1 +1,19 @@
+'''
+In this code we change the bots existing profile picture
+'''
 
+import nextcord
+from nextcord.ext import commands
+
+intents = nextcord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.command()
+async def changeimg(interaction: nextcord.Interaction):
+    with open('botpfp.png', 'rb') as image:
+        await bot.user.edit(avatar=image.read())
+        await interaction.send("Set new pfp")
+
+bot.run("TOKEN")
