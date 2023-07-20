b4b753fa74d79821b2044cf335332661785fc68f
Quazar <112936536+Quazar4@users.noreply.github.com>
2023-07-20 20:46:43 +0530

    Create DisplayRoles.py

---
+++ roles/DisplayRoles.py
@@ -0,0 +1,25 @@
+'''
+In this code we display all the roles of the member by looping through "ctx.author.roles" or "member.roles" stored as the roles varaible
+in this code
+'''
+
+
+import nextcord
+from nextcord.ext import commands
+
+intents = nextcord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.command()
+async def get_roles(ctx, member: nextcord.Member = None):
+    if member is None:
+        member = ctx.author
+
+    roles = member.roles
+    role_names = [role.name for role in roles]
+
+    await ctx.send(f"{member.mention} has the following roles: {', '.join(role_names)}")
+
+bot.run("YOUR_BOT_TOKEN")
